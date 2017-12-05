8a6467391ab0ed39c8e5faf91c91e770c6964ded
m0n0lith <p.soderlindh@gmail.com>
2017-12-05 21:59:48 +0100

    done?

--- day1/try1.py
+++ day1/try1.py
@@ -2,10 +2,43 @@
 # input of 1122 gives (1+2)=3
 # input of 976542319 gives 9
 # pos1 = pos1 -> pos1.value
-from argparse import ArgumentParser
-parser = ArgumentParser()
+import argparse
+parser = argparse.ArgumentParser()
 parser.add_argument('--file', '-f', default='', required=True)
+args = parser.parse_args()
 
 fileList = args.file
-print (fileList)
+print ('.:INPUT:.')
+f = open(args.file)
+inputStr = f.readline()
+inputStr = inputStr.strip('\n')
+print (inputStr+'\n')
+inputList = []
+#print(inputStr)
+inputLen = len(inputStr)
+#for i in inputStr:
+#    inputList.append(inputStr[i])
+result = 0
+for char in inputStr:
+    #print (char)
+    inputList.append(int(char))
 
+#inputList.append(inputList[0])
+print('.:LIST:.', '\n', inputList, '\n')
+
+for i in range(len(inputStr)-1):
+    if inputList[i] == inputList[i+1]:
+        result = result + inputList[i]
+        #print('split result: ' + str(result))
+    else:
+        result = result
+
+#print(inputList[-1])
+#print(inputList[0])
+if inputList[0] == inputList[-1]:
+    result = result + inputList[-1]
+else:
+    print('not equle')
+    result = result
+
+print('Sum: ' + str(result))
